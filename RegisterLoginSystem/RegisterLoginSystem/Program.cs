84357be40f74abe71ec9cbdcf0f655cae1983e3b
nqasanova <106176658+nqasanova@users.noreply.github.com>
2022-07-04 12:40:43 +0400

    Update Program.cs

--- RegisterLoginSystem/RegisterLoginSystem/Program.cs
+++ RegisterLoginSystem/RegisterLoginSystem/Program.cs
@@ -38,18 +38,6 @@ namespace PersonManagement
                     string secondPassword = Console.ReadLine();
 
                     RegisterUser(firstName, lastName, email, firstPassword, secondPassword);
-
-                    if (!IsEmailValid(email))
-                    {
-                        Console.WriteLine("You have entered wrong email!");
-                        continue;
-                    }
-
-                    else if (!IsPasswordValid(firstPassword, secondPassword))
-                    {
-                        Console.WriteLine("You have entered wrong password!");
-                        continue;
-                    }
                 }
 
                 else if (command == "/login")
@@ -63,25 +51,11 @@ namespace PersonManagement
                     Console.Write("Please enter password again : ");
                     string secondPassword = Console.ReadLine();
 
-                    if (!IsEmailValid(email))
-                    {
-                        Console.WriteLine("You have entered wrong email!");
-                        continue;
-                    }
+                    LoginUser(email, firstPassword, secondPassword);
 
-                    else if (!IsPasswordValid(firstPassword, secondPassword))
-                    {
-                        Console.WriteLine("You have entered wrong password!");
-                        continue;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("You successfully logged in!");
-                    }
                 }
 
-                else if(command == "/exit")
+                else if (command == "/exit")
                 {
                     Console.WriteLine("Thank you for using our website!");
                     break;
@@ -99,10 +73,9 @@ namespace PersonManagement
         {
             User user = new User(firstName, lastName, email, firstPassword, secondPassword);
 
-            string checkpassword = Console.ReadLine();
-
             if (firstPassword == secondPassword)
             {
+                users.Add(user);
                 Console.WriteLine("You successfully registered, now you can login with your new account!");
                 return true;
             }
@@ -116,64 +89,35 @@ namespace PersonManagement
 
         public static bool LoginUser(string email, string firstpassword, string secondPassword)
         {
-            UserLoginValidation user = new UserLoginValidation(email, firstpassword, secondPassword);
-
-            if (firstpassword == secondPassword)
+            UserLoginValidation userLogin = new UserLoginValidation(email, firstpassword, secondPassword);
             {
-                Console.WriteLine("You successfully logged in!");
-                return true;
-            }
-
-            else
-            {
-                Console.WriteLine("The information you have entered do not match the requirements, please try again.");
-                return false;
-            }
-        }
-
-        public static bool IsEmailValid(string email)
-        {
-            for (int i = 0; i < email.Length; i++)
-            {
-                if (email[i] == '@')
+                if (firstpassword == secondPassword)
                 {
+                    Console.WriteLine("You successfully logged in!");
+                    LoginConfirmation(email);
                     return true;
                 }
-            }
 
-            Console.WriteLine("Your email must contain '@'!");
-            return false;
+                else
+                {
+                    Console.WriteLine("The information you have entered do not match the requirements, please try again.");
+                    return false;
+                }
+            }
         }
 
-        public static bool IsPasswordValid(string firstPassword, string secondPassword)
+        public static void LoginConfirmation(string email)
         {
-            if (firstPassword == secondPassword)
+            for (int i = 0; i < users.Count; i++)
             {
-                return true;
-            }
-
-            else
-            {
-                Console.WriteLine("The passwords you have entered do not match!");
-                return false;
+                if (users[i].Email == email)
+                {
+                    Console.WriteLine($"Welcome to your account {users[i].FirstName} {users[i].LastName} ");
+                }
             }
         }
 
-        class UserLoginValidation
-        {
-            public string Email { get; private set; }
-            public string FirstPassword { get; private set; }
-            public string SecondPassword { get; private set; }
-
-            public UserLoginValidation(string email, string firstPassword, string secondPassword)
-            {
-                Email = email;
-                FirstPassword = firstPassword;
-                SecondPassword = secondPassword;
-            }
-        }
-
-        class User
+        class User : BaseValidator
         {
             public string FirstName { get; private set; } = "Super";
             public string LastName { get; private set; } = "Admin";
@@ -190,46 +134,99 @@ namespace PersonManagement
                 SecondPassword = secondPassword;
             }
 
-            class BaseValidator
-            {
-                public static bool IsTextLengthCorrect(string text, int startLength, int endLength)
-                    {
-                        if (!(text.Length > startLength && text.Length < endLength))
-                        {
-                            return false;
-                        }
+        }
 
+        class UserLoginValidation
+        {
+            public string Email { get; private set; }
+            public string FirstPassword { get; private set; }
+            public string SecondPassword { get; private set; }
+
+            public UserLoginValidation(string email, string firstPassword, string secondPassword)
+            {
+                Email = email;
+                FirstPassword = firstPassword;
+                SecondPassword = secondPassword;
+            }
+        }
+
+        class BaseValidator
+        {
+            public static bool Validator(User user, List<User> users)
+            {
+                return IsFirstNameValid(user.FirstName) && IsLastNameValid(user.LastName) && IsEmailValid(user.Email) && /*IsEmailUnique(user.Email, users) &&*/ IsPasswordValid(user.FirstPassword, user.SecondPassword);
+            }
+
+            public static bool IsFirstNameValid(string FirstName)
+            {
+                if (!IsTextLengthCorrect(FirstName, 3, 30))
+                {
+                    Console.WriteLine("Entered first name does not match the requirements!");
+                    return false;
+                }
+
+                return true;
+            }
+
+            public static bool IsLastNameValid(string lastName)
+            {
+                if (!IsTextLengthCorrect(lastName, 5, 20))
+                {
+                    Console.WriteLine("Entered last name does not match the requirements!");
+                    return false;
+                }
+
+                return true;
+            }
+            public static bool IsTextLengthCorrect(string text, int startLength, int endLength)
+            {
+                if (!(text.Length > startLength && text.Length < endLength))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            public static bool IsEmailValid(string email)
+            {
+                for (int i = 0; i < email.Length; i++)
+                {
+                    if (email[i] == '@')
+                    {
                         return true;
                     }
                 }
 
-            class UserValidator : BaseValidator
+                Console.WriteLine("Your email must contain '@'!");
+                return false;
+            }
+
+            /*public static bool IsEmailUnique(string email, List<User> users)
             {
-                public static bool Validator(User user, List<User> users)
+                for (int i = 0; i < users.Count; i++)
                 {
-                    return IsFirstNameValid(user.FirstName) && IsLastNameValid(user.LastName) && IsEmailValid(user.Email) && IsPasswordValid(user.FirstPassword, user.SecondPassword);
-                }
-
-                public static bool IsFirstNameValid(string FirstName)
-                {
-                    if (!IsTextLengthCorrect(FirstName, 3, 30))
+                    if (users[i].Email == email)
                     {
-                        Console.WriteLine("Entered first name does not match the requirements!");
+                        Console.WriteLine("This email has already been registered, try to register with another email!");
                         return false;
                     }
 
                     return true;
                 }
+            }*/
 
-                public static bool IsLastNameValid(string lastName)
+            public static bool IsPasswordValid(string firstPassword, string secondPassword)
+            {
+                if (firstPassword == secondPassword)
                 {
-                    if (!IsTextLengthCorrect(lastName, 5, 20))
-                    {
-                        Console.WriteLine("Entered last name does not match the requirements!");
-                        return false;
-                    }
-
                     return true;
+                }
+
+                else
+                {
+                    Console.WriteLine("The passwords you have entered do not match!");
+                    return false;
                 }
             }
         }
